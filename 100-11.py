88c8f790bd3b6a6864de2ad87f536716a45f277e
whyismefly <whyismeflying@hotmail.com>
2017-07-26 09:28:59 +0800

    1

--- 100-11.py
+++ 100-11.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
 #encoding:utf-8
 #  Python练习题要求如下：
+#简述：话说有一对可爱的兔子，出生后的第三个月开始，每一月都会生一对小兔子。当小兔子长到第三
+# 个月后，也会每个月再生一对小小兔子.
 # 问题：假设条件，兔子都不死的情况下，问每个月的兔子总数为多少？
 
-month=0
-rabit=2
+# month=int(raw_input("pls input time"))
+# birth=month/3
+#
+# for i in range(birth):
+#     rabit=
+
+a1 = 1
+b2 = 1
+for i in range(1, 10):
+        print '%12ld %12ld' % (a1, b2),
+        if (i % 3) == 0:
+            print ''
+        a1 = a1 + b2
+        b2 = a1 + b2
