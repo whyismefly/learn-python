d05604a48a5a47853e47a15a6fb1f1b41e93e31b
whyismefly <whyismeflying@hotmail.com>
2017-07-26 23:18:51 +0800

    0726

---
+++ 100-1.py
@@ -0,0 +1,13 @@
+#!usr/bin/python
+#encodeing:utf-8
+# Python练习题问题如下：
+# 简述：这里有四个数字，分别是：1、2、3、4
+# 提问：能组成多少个互不相同且无重复数字的三位数？各是多少？
+x=0
+for i in range(1,5):
+    for j in range(1,5):
+        for k in range(1,5):
+            if(i!=j)and(i!=k)and(k!=j):
+                print(i,j,k)
+                x+=1
+print(x)
