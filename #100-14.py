b466f3eb655063912a74f2bc0f6501fa0fdf22d4
whyismefly <whyismeflying@hotmail.com>
2017-07-29 01:53:37 +0800

    29

--- #100-14.py
+++ #100-14.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #encoding:utf-8
 
 # 题目：将一个正整数分解质因数。例如：输入90,打印出90=2*3*3*5。
@@ -8,35 +8,51 @@
 # (2)如果n<>k，但n能被k整除，则应打印出k的值，并用n除以k的商,作为新的正整数你n,重复执行第一步。
 # (3)如果n不能被k整除，则用k+1作为k的值,重复执行第一步。
 
+
+import math
 #关于如何跳出循环重2开始的哪一点没有弄明白
 
-def reduceNum(n):
-    print '{} = '.format(n),
-    if not isinstance(n, int) or n <= 0 :
-        print '请输入一个正确的数字 !'
-        exit(0)
-    elif n in [1] :
-        print '{}'.format(n)
-    while n not in [1] : # 循环保证递归
-        for index in xrange(2, n + 1) :
-			#xrange这个迭代器每次都是从头开始迭代，不像range继续迭代
-            if n % index == 0:
-                n /= index # n 等于 n/index
-                if n == 1:
-                    print index
-                else : # index 一定是素数
-                    print '{} *'.format(index),
-                break
-
-def getnum(n):
-	if n<0:
-		print"wrong number"
-	elif n in[1]:
-		print n
-	for n in xrange(2,n+1):
-		if
-
-getnum(2)
-
-# x=input("num：\n")
-# reduceNum(x)
+# def reduceNum(n):
+#     print '{} = '.format(n),
+#     if not isinstance(n, int) or n <= 0 :
+#         print '请输入一个正确的数字 !'
+#         exit(0)
+#     elif n in [1] :
+#         print '{}'.format(n)
+#     while n not in [1] : # 循环保证递归
+#         for index in xrange(2, n + 1) :
+# 			#xrange这个迭代器每次都是从头开始迭代，不像range继续迭代
+#           #在python3中xrange就是range
+#             if n % index == 0:
+#                 n /= index # n 等于 n/index
+#                 if n == 1:
+#                     print index
+#                 else : # index 一定是素数
+#                     print '{} *'.format(index),
+#                 break
+# n=int(input("num"))
+n=int(16)
+if n<0:
+		print("wrong number")
+elif n in[1]:
+		print (n)
+y=1
+#引入y作为对输入的n是否继续检测做判断
+while(y):
+    for i in range(2,int(math.sqrt(n)+2)):
+    # for i in range(2, n + 1):
+     if  n % i == 0:
+        n=int(n/i);
+        print(i)
+        print(" ")
+        break
+        if n==1:
+            break
+        else:
+            y = 0
+    else :
+        y=0
+        if n!=1:
+            print(n)
+        else:
+            print("sushu")
