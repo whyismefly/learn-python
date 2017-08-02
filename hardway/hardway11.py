73a3ac03de316485524d0450abb7d0ff7ac4d8f1
whyismefly <whyismeflying@hotmail.com>
2017-08-02 17:56:01 +0800

    0802
    hardway11

--- hardway/hardway11.py
+++ hardway/hardway11.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python
 # encoding:utf-8
 
+print "how old are you",
+age=raw_input()
+print "how tall"
+height=raw_input()
+print "weight",
+weight=raw_input()
+
+print "you are %s old,%s tall and %s heavy"%(age,height,weight)
