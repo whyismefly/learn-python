4c966e77ee90df81b7216c28a4f1c80cabd439c8
whyismefly <whyismeflying@hotmail.com>
2017-08-07 23:59:06 +0800

    0807
    hard25-28

--- hardway/hardway25.py
+++ hardway/hardway25.py
@@ -3,8 +3,12 @@
 
 def break_words(stuff):
     """break up"""
-    words=stuff.split(' ')
+    words=stuff.split()
+    #words = stuff.split(' ')
     return  words
+"""
+遇到问题'list' object has no attribute 'split'
+"""
 
 def sort_words(words):
     """sort words"""
@@ -36,3 +40,65 @@ def print_first_and_last_sorted(sentence):
     words=sort_sentence(sentence)
     print_first_words(words)
     print_last_words(words)
+
+# Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (
+# Intel)] on win32
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> import hardway25
+# >>> sentence="uyhiyh jhuiohn mnom omo "
+# >>> words=hardway.break_words(sentence)
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# NameError: name 'hardway' is not defined
+# >>> words=hardway25.break_words(sentence)
+# >>> words
+# ['uyhiyh', 'jhuiohn', 'mnom', 'omo', '']
+# >>> sorted_words=hardway25.sort_words(words)
+# >>> sorted_words
+# ['', 'jhuiohn', 'mnom', 'omo', 'uyhiyh']
+# >>>
+# >>> hardway25.print_first_word(words)
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# AttributeError: 'module' object has no attribute 'print_first_word'
+# >>> hardway25.print_first_words(words)
+# uyhiyh
+# >>> hardway25.break_words(sentence)
+# ['uyhiyh', 'jhuiohn', 'mnom', 'omo', '']
+# >>> help(ex25
+# ... help(ex25)
+#   File "<stdin>", line 2
+#     help(ex25)
+#        ^
+# SyntaxError: invalid syntax
+# >>> help(ex25)
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# NameError: name 'ex25' is not defined
+# >>> help(hardway25)
+# Help on module hardway25:
+#
+# NAME
+#     hardway25
+#
+# FILE
+#     d:\python\python27\hardway25.pyc
+#
+# FUNCTIONS
+#     break_words(stuff)
+#         break up
+#
+#     print_first_and_last(sentence)
+#         print_first_and_last words in sentence
+#
+#     print_first_and_last_sorted(sentence)
+#         print_first_and_last_sorted in sentence
+#
+#     print_first_words(words)
+#         print_first_words after poping
+#
+#     print_last_words(words)
+#         print_last_words after poping
+#
+# -- More  --
+
