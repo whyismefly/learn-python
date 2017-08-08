e0d82b035f44577cb2c691bf17cd9fb8614e95e6
whyismefly <whyismeflying@hotmail.com>
2017-08-09 00:51:34 +0800

    170809
    hard35

--- hardway/hardway34.py
+++ hardway/hardway34.py
@@ -4,3 +4,4 @@
 animals=['bear','tiger','penguin','zebra']
 bear=animals[0]
 #Remember: ordinal == ordered, 1st; cardinal == cards at random, 0.
+animals=['bear','python','penguin','kangaroo','whale','platypus']
