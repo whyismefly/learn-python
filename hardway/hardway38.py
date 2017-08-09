2add08228d1c15797961d218f2d39421f31ce23b
whyismefly <whyismeflying@hotmail.com>
2017-08-09 18:21:26 +0800

    170809
    hard38

--- hardway/hardway38.py
+++ hardway/hardway38.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 # encoding:utf-8
 
+str = "-";
+seq = ("a", "b", "c"); # 字符串序列
+print str.join( seq );
+
 ten_things = "Apples Oranges Crows Telephone Light Sugar"
 print "Wait there's not 10 things in that list, let's fix that."
 
 stuff=ten_things.split(' ')
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
-
-print stuff
+print stuff,"\n"
 while len(stuff)!=10:
     next_one=more_stuff.pop()
+    #从末位pop
+    print more_stuff
     print "add:",next_one
     stuff.append(next_one)
     print "%d items now"%len(stuff)
