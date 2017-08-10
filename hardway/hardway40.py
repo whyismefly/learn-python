b8fd942674ceda8ab6dda795af1426b7adc0a709
whyismefly <whyismeflying@hotmail.com>
2017-08-11 01:31:52 +0800

    170811
    hard40.

--- hardway/hardway40.py
+++ hardway/hardway40.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python
 # encoding:utf-8
+
+class MyStuff(object):
+    def __init__(self):
+        self.tangerine="And now a thousand years between"
+
+    def apple(self):
+        print "classy apples"
