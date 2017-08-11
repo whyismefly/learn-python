abd4a92ae09545e3184578be4f7934a497435245
whyismefly <whyismeflying@hotmail.com>
2017-08-11 19:08:09 +0800

    170811
    hard41&oop

--- hardway/hardway40.py
+++ hardway/hardway40.py
@@ -7,3 +7,28 @@ class MyStuff(object):
 
     def apple(self):
         print "classy apples"
+
+thing=MyStuff()
+thing.apple()
+print thing.tangerine
+print "\n"
+
+class Song(object):
+    def __init__(self,lyrics):
+        self.lyrics=lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print line
+
+happy_bday=Song(["Happy birthday to you",
+"I don't want to get sued",
+"So I'll stop right there"])
+
+bulls_on_parade=Song(["They rally around the family",
+"With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+print '-'*20
+
+bulls_on_parade.sing_me_a_song()
