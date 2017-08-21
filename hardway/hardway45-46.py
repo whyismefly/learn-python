3ac4258dbb1383139c22a88ddfc25575f22b4d8e
whyismefly <whyismeflying@hotmail.com>
2017-08-21 16:32:07 +0800

    0821
    HARD-46

--- hardway/hardway45-46.py
+++ hardway/hardway45-46.py
@@ -268,3 +268,193 @@ Finished processing dependencies for virtualenv==15.1.0
 D:\PYTHON\virtualenv-15.1.0\virtualenv-15.1.0>
 
 """
+
+#-------------------------------------------------------------------------
+"""
+cmd进到指定位置后使用指令创建skeleton框架 
+"""
+"""
+mkdir projects
+cd projects/
+mkdir skeleton
+cd skeleton
+mkdir bin
+mkdir NAME
+mkdir tests
+mkdir docs
+"""
+#-------------------------------------------------------------------------
+"""
+powershell中运行以下创建指令
+
+new-item -type file NAME/__init__.py
+new-item -type file tests/__init__.py
+"""
+#-------------------------------------------------------------------------
+
+"""
+Windows PowerShell
+版权所有 (C) 2009 Microsoft Corporation。保留所有权利。
+
+PS C:\Users\Administrator> cd E:\GITHUBWORK\learn-python\projects\skeleton
+PS E:\GITHUBWORK\learn-python\projects\skeleton> new-item -type file NAME/__init__.py
+    目录: E:\GITHUBWORK\learn-python\projects\skeleton\NAME
+Mode                LastWriteTime     Length Name
+----                -------------     ------ ----
+-a---         2017/8/21     16:02          0 __init__.py
+
+
+PS E:\GITHUBWORK\learn-python\projects\skeleton> $new-item -type file tests/__init__.py
+必须在“-”运算符的右侧提供值表达式。
+所在位置 行:1 字符: 6
++ $new- <<<< item -type file tests/__init__.py
+    + CategoryInfo          : ParserError: (:) [], ParentContainsErrorRecordException
+    + FullyQualifiedErrorId : ExpectedValueExpression
+
+PS E:\GITHUBWORK\learn-python\projects\skeleton> $ new-item -type file tests/__init__.py
+无法将“$”项识别为 cmdlet、函数、脚本文件或可运行程序的名称。请检查名称的拼写，如果包括路径，请确保路径正确，然后重试
+。
+所在位置 行:1 字符: 2
++ $ <<<<  new-item -type file tests/__init__.py
+    + CategoryInfo          : ObjectNotFound: ($:String) [], CommandNotFoundException
+    + FullyQualifiedErrorId : CommandNotFoundException
+
+PS E:\GITHUBWORK\learn-python\projects\skeleton> new-item -type file tests/__init__.py
+
+
+    目录: E:\GITHUBWORK\learn-python\projects\skeleton\tests
+
+
+Mode                LastWriteTime     Length Name
+----                -------------     ------ ----
+-a---         2017/8/21     16:05          0 __init__.py
+
+
+PS E:\GITHUBWORK\learn-python\projects\skeleton> ls -R
+
+
+    目录: E:\GITHUBWORK\learn-python\projects\skeleton
+
+
+Mode                LastWriteTime     Length Name
+----                -------------     ------ ----
+d----         2017/8/21     15:32            bin
+d----         2017/8/21     15:32            docs
+d----         2017/8/21     16:02            NAME
+d----         2017/8/21     16:05            tests
+
+
+    目录: E:\GITHUBWORK\learn-python\projects\skeleton\NAME
+
+
+Mode                LastWriteTime     Length Name
+----                -------------     ------ ----
+-a---         2017/8/21     16:02          0 __init__.py
+
+
+    目录: E:\GITHUBWORK\learn-python\projects\skeleton\tests
+
+
+Mode                LastWriteTime     Length Name
+----                -------------     ------ ----
+-a---         2017/8/21     16:05          0 __init__.py
+
+
+PS E:\GITHUBWORK\learn-python\projects\skeleton> ls -Rcd
+Get-ChildItem : 找不到与参数名称“Rcd”匹配的参数。
+所在位置 行:1 字符: 8
++ ls -Rcd <<<<
+    + CategoryInfo          : InvalidArgument: (:) [Get-ChildItem], ParameterBindingException
+    + FullyQualifiedErrorId : NamedParameterNotFound,Microsoft.PowerShell.Commands.GetChildItemCommand
+
+PS E:\GITHUBWORK\learn-python\projects\skeleton> cd
+PS E:\GITHUBWORK\learn-python\projects\skeleton>
+PS E:\GITHUBWORK\learn-python\projects\skeleton> cd E:\GITHUBWORK\learn-python\projects
+PS E:\GITHUBWORK\learn-python\projects> ls -R
+
+
+    目录: E:\GITHUBWORK\learn-python\projects
+
+
+Mode                LastWriteTime     Length Name
+----                -------------     ------ ----
+d----         2017/8/21     15:32            skeleton
+-a---         2017/8/21     16:12        438 setup.py
+-a---         2017/8/21     16:08          0 __init__.py
+
+
+    目录: E:\GITHUBWORK\learn-python\projects\skeleton
+
+
+Mode                LastWriteTime     Length Name
+----                -------------     ------ ----
+d----         2017/8/21     15:32            bin
+d----         2017/8/21     15:32            docs
+d----         2017/8/21     16:02            NAME
+d----         2017/8/21     16:17            tests
+
+
+    目录: E:\GITHUBWORK\learn-python\projects\skeleton\NAME
+
+
+Mode                LastWriteTime     Length Name
+----                -------------     ------ ----
+-a---         2017/8/21     16:02          0 __init__.py
+
+
+    目录: E:\GITHUBWORK\learn-python\projects\skeleton\tests
+
+
+Mode                LastWriteTime     Length Name
+----                -------------     ------ ----
+-a---         2017/8/21     16:17        150 NAME_tests.py
+-a---         2017/8/21     16:05          0 __init__.py
+
+
+PS E:\GITHUBWORK\learn-python\projects> cd .\skeleton
+PS E:\GITHUBWORK\learn-python\projects\skeleton> cd ..
+PS E:\GITHUBWORK\learn-python\projects> ls
+
+
+    目录: E:\GITHUBWORK\learn-python\projects
+
+
+Mode                LastWriteTime     Length Name
+----                -------------     ------ ----
+d----         2017/8/21     15:32            skeleton
+-a---         2017/8/21     16:12        438 setup.py
+-a---         2017/8/21     16:08          0 __init__.py
+
+
+PS E:\GITHUBWORK\learn-python\projects> nosetests
+
+----------------------------------------------------------------------
+Ran 0 tests in 0.000s
+
+OK
+PS E:\GITHUBWORK\learn-python\projects> nosetests
+E
+======================================================================
+ERROR: Failure: SyntaxError (invalid syntax (NAME_tests.py, line 2))
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "D:\PYTHON\Python27\lib\site-packages\nose-1.3.7-py2.7.egg\nose\loader.py", line 418, in loadTestsFromName
+    addr.filename, addr.module)
+  File "D:\PYTHON\Python27\lib\site-packages\nose-1.3.7-py2.7.egg\nose\importer.py", line 47, in importFromPath
+    return self.importFromDir(dir_path, fqname)
+  File "D:\PYTHON\Python27\lib\site-packages\nose-1.3.7-py2.7.egg\nose\importer.py", line 94, in importFromDir
+    mod = load_module(part_fqname, fh, filename, desc)
+  File "E:\GITHUBWORK\learn-python\projects\skeleton\tests\NAME_tests.py", line 2
+    import
+         ^
+SyntaxError: invalid syntax
+
+----------------------------------------------------------------------
+Ran 1 test in 0.037s
+
+FAILED (errors=1)
+PS E:\GITHUBWORK\learn-python\projects>
+
+
+
+"""
