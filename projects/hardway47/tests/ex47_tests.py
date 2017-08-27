8003b0bbc33fd96616e29f922cd6be55b289ddb0
whyismefly <whyismeflying@hotmail.com>
2017-08-28 01:29:31 +0800

    0828

--- projects/hardway47/tests/ex47_tests.py
+++ projects/hardway47/tests/ex47_tests.py
@@ -1,32 +1,67 @@
+# from nose.tools import *
+# from ex47.game import Room
+#
+#
+# def test_room() :
+#     gold=Room("goldroom",
+#               """golds in.
+#               a door north""")
+#     assert_equal(gold.name,"goldroom")
+#     assert_equal(gold.paths,[])
+#
+# def test_room_paths():
+#     center=Room("center","test room in the center")
+#     north=Room("North","test room in north")
+#     south=Room("South","test room in south")
+#
+#     center.add_paths(['north':north,'south':south])
+#     assert_equal(center.go("north"),north)
+#     assert_equal(center.go('south'),south)
+#
+# def test_map():
+#     start=Room("start","go west and down a hole")
+#     west=Room("trees","trees here,go east")
+#     down=Room("dungeon","dark here,go up")
+#
+#     start.add_paths(['west': west, 'down': down])
+#     west.add_paths(['east':start])
+#     down.add_paths(['up': start])
+#
+#     assert_equal(start.go('west'),west)
+#     assert_equal(start.go('west').go('east'),start)
+#     assert_equal(start.go('down').go('up'),start)
+
 from nose.tools import *
+
+import nose
+import sys
+# sys.path.append("../ex47")
+# 如果不用ex47.game则需要加上这句
+
 from ex47.game import Room
 
-
-def test_room() :
-    gold=Room("goldroom",
-              """golds in.
-              a door north""")
-    assert_equal(gold.name,"goldroom")
-    assert_equal(gold.paths,[])
-
+def test_room():
+    gold = Room("GoldRoom", """This room has gold in it you can grab. There's a
+            door to the north.""")
+    assert_equal(gold.name, "GoldRoom")
+    assert_equal(gold.paths, {})
 def test_room_paths():
-    center=Room("center","test room in the center")
-    north=Room("North","test room in north")
-    south=Room("South","test room in south")
-
-    center.add_paths(['north':north,'south':south])
-    assert_equal(center.go("north"),north)
-    assert_equal(center.go('south'),south)
-
+    center = Room("Center", "Test room in the center.")
+    north = Room("North", "Test room in the north.")
+    south = Room("South", "Test room in the south.")
+    center.add_paths({'north': north, 'south': south})
+    assert_equal(center.go('north'), north)
+    assert_equal(center.go('south'), south)
 def test_map():
-    start=Room("start","go west and down a hole")
-    west=Room("trees","trees here,go east")
-    down=Room("dungeon","dark here,go up")
+    start = Room("Start", "You can go west and down a hole.")
+    west = Room("Trees", "There are trees here, you can go east.")
+    down = Room("Dungeon", "It's dark down here, you can go up.")
+    start.add_paths({'west': west, 'down': down})
+    west.add_paths({'east': start})
+    down.add_paths({'up': start})
+    assert_equal(start.go('west'), west)
+    assert_equal(start.go('west').go('east'), start)
+    assert_equal(start.go('down').go('up'), start)
 
-    start.add_paths(['west': west, 'down': down])
-    west.add_paths(['east':start])
-    down.add_paths(['up': start])
-
-    assert_equal(start.go('west'),west)
-    assert_equal(start.go('west').go('east'),start)
-    assert_equal(start.go('down').go('up'),start)
+if __name__ == "__main__":
+    nose.runmodule()
