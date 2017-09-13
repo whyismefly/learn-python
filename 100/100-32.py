d19f05694879e9db7d68886748f7e15f78ba0332
whyismefly <whyismeflying@hotmail.com>
2017-09-14 00:40:39 +0800

    170914
    100-31-32

---
+++ 100/100-32.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# encoding:utf-8
+
+# 题目：按相反的顺序输出列表的值。
+# 程序分析：无。
+
+def list(time):
+    lists=[]
+    for i in range(time):
+        word=raw_input("input list:")
+        lists.append(word)
+    return lists
+
+def last2first(list):
+    for i in range(len(list)):
+        print list[-1-i]
+
+time=int(raw_input("how many words:"))
+lis=list(time)
+print "-"*20
+last2first(lis)
+
+
---
+++ 100/100-33.py
@@ -0,0 +1,4 @@
+time=3
+for i in range(time):
+    word = raw_input("input list :"),time
+    print word
