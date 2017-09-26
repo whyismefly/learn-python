8690bde2d6d83187b049de41c9ef8a449239e49a
whyismefly <whyismeflying@hotmail.com>
2017-09-26 15:15:17 +0800

    20170926
    100-39

--- 100/100-39.py
+++ 100/100-39.py
@@ -4,13 +4,16 @@
 # 题目：有一个已经排好序的数组。现输入一个数，要求按原来的规律将它插入数组中。
 # 程序分析：首先判断此数是否大于最后一个数，然后再考虑插入中间的数的情况，插入后此元素之后的数，依次后移一个位置。
 
-num=[165,165,63,654,21,6,516,415,65,56]
-num.sort()
-x=18
-list=[]
+num=[6, 21, 56, 63, 65, 165, 165, 415, 516, 463,463,463,464,464,464,464,654]
+x=463
+print num
 for i in range(len(num)):
-    if x>num[i]:
-        list[i]=num[i]
+    if x>num[-1-i]:
+        num.insert(-i+1,x)
         break
-    else:
-        list.append(x)
+print num
+
+
+list=[1,2,3]
+list.insert(3,x)
+print list
