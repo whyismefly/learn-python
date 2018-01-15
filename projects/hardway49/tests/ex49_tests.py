adf8bf31bad36c6715c6a09bab35eb6666b873e2
whyismefly <whyismeflying@hotmail.com>
2018-01-15 17:29:23 +0800

    20180115
    hardway49

---
+++ projects/hardway49/tests/ex49_tests.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# encoding:utf-8
+
+class PaserError(Exception):
+    pass
+
+class Sentence(object):
+
+    def __init__(self,subject,verb,obj):
+
+        self.subject=subject[1]
+        self.verb=verb[1]
+        self.object=obj[1]
+
+    def peek(word_list):
+        if word_list:
+            word=word_list[0]
+            return  word[0]
+        else:
+            return None
+
+    def match(word_list,expecting):
+        if word_list:
+            word=word_list.pop(0)
+
+            if word[0]==expecting:
+                return word
+            else:
+                return None
+        else:
+            return None
+
+    def skip(word_list,word_type):
+        while peek(word_list)==word_type:
+            match(word_list,word_type)
