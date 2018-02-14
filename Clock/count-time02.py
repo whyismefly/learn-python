5c5a65946e1057680cd001005197a71bc3123d11
whyismefly <whyismeflying@hotmail.com>
2018-02-14 09:15:35 +0800

    20180204

--- Clock/count-time02.py
+++ Clock/count-time02.py
@@ -5,9 +5,10 @@ import time
 while True:
         t = time.localtime()
 
-        time.sleep(0.4)
+        time.sleep(1)
         t1=time.localtime()
         if t!=t1:
-            print("\r现在时刻: %d 年 %2d 月 %2d日 %2d 时 %2d 分 %2d 秒 星期 %d") \
+            print("现在时刻: %d 年 %2d 月 %2d日 %2d 时 %2d 分 %2d 秒 星期 %d\r") \
                  % (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec, t.tm_wday + 1)
+
 
