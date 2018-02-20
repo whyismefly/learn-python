a506debc1521594469499dc4fbffc449f32aa30d
whyismefly <whyismeflying@hotmail.com>
2018-02-20 12:36:30 +0800

    20180220

--- 爬虫学习/stock/stock01.py
+++ 爬虫学习/stock/stock01.py
@@ -2,11 +2,34 @@
 # encoding:utf-8
 
 import urllib
+import sqlite3
 
-def getHtml(url):
-    page = urllib.urlopen(url)
-    html = page.read()
-    return html
-
+#获取网页
 def gethtml(url):
     page =urllib.urlopen(url)
+    html=page.read()
+    return html
+
+
+# 保存网页
+def savehtmlastxt(html):
+    html = open("1.html", "wb")
+    try:
+        html.write(h)
+    finally:
+        html.close()
+
+h = gethtml("https://www.cnblogs.com/snowbook/p/5764549.html")
+savehtmlastxt(h)
+
+
+
+# try:
+#     f = open('/path/to/file', 'r')
+#     print f.read()
+# finally:
+#     if f:
+#         f.close()
+#
+# with open('/path/to/file', 'r') as f:
+#     print f.read()
