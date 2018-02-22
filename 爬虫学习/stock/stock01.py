691e1a7cf44ee59cc826b298747efc5ba5a82de6
whyismefly <whyismeflying@hotmail.com>
2018-02-23 00:56:47 +0800

    20180223

--- 爬虫学习/stock/stock01.py
+++ 爬虫学习/stock/stock01.py
@@ -12,18 +12,39 @@ def gethtml(url):
 
 
 # 保存网页
-def savehtmlastxt(html):
-    html = open("1.html", "wb")
+def savehtmlastxt(html,savename):
+    # html = open(str(savename)+".html", "wb")
+    #中文会出现乱码
+    html = open(unicode(str(savename) + ".html","utf-8"), "wb")
     try:
         html.write(h)
     finally:
         html.close()
 
-h = gethtml("https://www.cnblogs.com/snowbook/p/5764549.html")
-savehtmlastxt(h)
+#提取网页信息/正则
+def selectinfomation(html):
+    print "hello"
 
+#测试
+#百度股票
+# h = gethtml("https://www.cnblogs.com/snowbook/p/5764549.html")
+#东方财富网
+h = gethtml("http://quote.eastmoney.com/stocklist.html")
+savehtmlastxt(h,"股票名称编号")
 
+#遍历文件夹并获取特定类型文件
+# import os
+#
+#
+# def file_name(file_dir):
+#     L = []
+#     for root, dirs, files in os.walk(file_dir):
+#         for file in files:
+#             if os.path.splitext(file)[1] == '.jpeg':
+#                 L.append(os.path.join(root, file))
+#     return L
 
+#获取指定文件内容
 # try:
 #     f = open('/path/to/file', 'r')
 #     print f.read()
