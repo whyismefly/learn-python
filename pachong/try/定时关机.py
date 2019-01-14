04e1334dcfdd761bbeae89889113284ec4c62490
whyismefly <whyismeflying@hotmail.com>
2019-01-15 01:54:37 +0800

    Update 定时关机.py

--- pachong/try/定时关机.py
+++ pachong/try/定时关机.py
@@ -2,12 +2,19 @@
 # -*- coding: UTF-8 -*-
 """参考https://pypi.org/project/schedule/
 python提供标准库sched也可以用，没看"""
-import schedule,os,time,datetime
+
+"""多进程参考https://www.cnblogs.com/znicy/p/6279930.html
+http://www.cnblogs.com/math98/p/8980916.html
+multiprocessing这个复杂一些，https://www.cnblogs.com/kaituorensheng/p/4445418.html"""
+
+
+import schedule,os,time,datetime,sys,multiprocessing
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 def jobwarn():
     t=time.now = datetime.datetime.now()
     print t,"注意时间，早点休息。"
-
 def job1s():
     print "1秒后关机"
     os.system("shutdown -s -t 1")
@@ -20,33 +27,41 @@ def job60s():
 def job120s():
     print "120s后关机"
     os.system("shutdown -s -t 120")
+def job1_goon():
+    print "11111111111111111111111111111111"
+    while True:
+        info=raw_input("more:")
+        if info == "s":
+            print info, "再续一会"
+            os.system("shutdown -a")
+        else:
+            print "as plan"
+def my_list():
+    schedule.every().day.at("23:30").do(jobwarn)
+    schedule.every().day.at("23:58").do(job120s)
+    schedule.every().day.at("00:30").do(job5s)
+    schedule.every().day.at("00:45").do(job5s)
+    schedule.every().day.at("01:00").do(job5s)
+    schedule.every().day.at("01:15").do(job5s)
+    schedule.every().day.at("01:28").do(job120s)
+    schedule.every().day.at("01:45").do(job5s)
+    schedule.every().day.at("02:00").do(job1s)
+    schedule.every().day.at("02:15").do(job1s)
+    schedule.every().day.at("02:30").do(job1s)
+    schedule.every().day.at("02:45").do(job1s)
+    # schedule.every(5).seconds.do(jobwarn)
+    print "222222222222222222222222222"
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
-def job_goon():
-    print "再续一会"
-    os.system("shutdown -a")
-def job1_goon(info):
-    print info,"再续一会"
-    os.system("shutdown -a")
 
-info="这是使用带参数的schedule方法；"
 
-schedule.every().day.at("23:30").do(jobwarn)
-schedule.every().day.at("23:58").do(job120s)
-schedule.every().day.at("00:30").do(job5s)
-schedule.every().day.at("00:45").do(job5s)
-schedule.every().day.at("01:00").do(job5s)
-schedule.every().day.at("01:15").do(job5s)
-schedule.every().day.at("01:28").do(job120s)
-schedule.every().day.at("01:45").do(job5s)
-schedule.every().day.at("02:00").do(job1s)
-schedule.every().day.at("02:15").do(job1s)
-schedule.every().day.at("02:30").do(job1s)
-schedule.every().day.at("02:45").do(job1s)
+if __name__ == "__main__":
+    p1 = multiprocessing.Process(target = my_list(), args = (2,))
+    p2 = multiprocessing.Process(target = job1_goon(), args = (3,))
+    p1.start()
+    p2.start()
 
-# schedule.every(5).seconds.do(jobwarn)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-
+    print "OK"
 
