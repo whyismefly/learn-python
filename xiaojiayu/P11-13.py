59f438c03698e9f1e9bc5348007c2c5e7b937841
whyismefly <whyismeflying@hotmail.com>
2019-02-13 00:44:21 +0800

    Update P11-13.py

--- xiaojiayu/P11-13.py
+++ xiaojiayu/P11-13.py
@@ -28,4 +28,38 @@ print member[1:3]
 print member[:]
 #切片
 print member[:3]
-
+list1=[123]
+list2=[1234]
+print list1>list2
+list1=[123,465]
+list2=[1234,111]
+list3=[123,465]
+print (list1<list2)and(list1==list3)
+list4=list1+list2
+print list4
+list4 *= 4
+print list4
+print "dfwgfiuofrwefrgrhtyjgh" not in list4
+print "123" not in list4
+list5=[123,["nfiewufhehopi","hferhfgioehrg"],16516]
+print 123 not in list5
+print 123 not in list5[1]
+print list5[1][1]
+print list4.count(123)
+print list4.count("123")
+print list4.index(123)
+#设置查询范围
+print list4.index(123,3,7)
+print list4
+list4.reverse()
+print list4
+list4.sort()
+print list4
+list4.sort(reverse=True)
+print list4
+#切片拷贝的区别
+list5=list4[:]
+list6=list4
+list4.sort()
+print list5
+print list6
