54619790400afda774d90a47a03dcfe69b439ee1
whyismefly <whyismeflying@hotmail.com>
2019-02-12 23:51:08 +0800

    20190212

---
+++ xiaojiayu/P11.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# encoding:utf-8
+
+member=['fbwifhb','frejihg','frhoiuhrfe','feuhgghuiwthgrkpogrkj']
+print member
+#末尾添加一个元素
+member.append("hncfjkelwv fev heh w")
+print member
+#添加多个
+member.extend(["hncfjkelwv fev heh w","feruiowrhjgopr"])
+print member
+print len(member)
+#插入
+member.insert(0,11111111111)
+print member
