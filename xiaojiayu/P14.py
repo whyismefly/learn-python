0019cc3a7c97876d715555dc0e785ff5303023a0
whyismefly <whyismeflying@hotmail.com>
2019-02-14 01:10:26 +0800

    20190212-1

---
+++ xiaojiayu/P14.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# encoding:utf-8
+
+#元组  不可直接修改  字符串类似于元组
+tuple1=(1,2,3,4,5,6,7,8,9,)
+print tuple1[5:]
+print tuple1[:5]
+temp=1
+print type(temp)
+temp2=25,3,4
+print type(temp2)
+temp=1,
+print type(temp)
+temp=(1,)
+print type(temp)
+temp=("fwhruifhei",2123123,"fhoierqwhfrei,","fhuirhfgrfgggbmkkh")
+temp=temp[:2]+("jfowhjf",)+temp[2:]
+print temp
