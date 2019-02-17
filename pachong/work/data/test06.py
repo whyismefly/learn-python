e191e038d31d4b3cf73a32047627a490857c52d7
whyismefly <whyismeflying@hotmail.com>
2019-02-18 00:49:32 +0800

    20190218

--- pachong/work/data/test06.py
+++ pachong/work/data/test06.py
@@ -26,3 +26,23 @@ rect = Rectangle1(3,4)
 print(rect.getPeri())
 print(rect.getArea())
 print(rect.__dict__)
+
+class Bird(object):
+    def __init__(self):
+          self.hungry = True
+    def eat(self):
+          if self.hungry:
+               print 'Ahahahah'
+          else:
+               print 'No thanks!'
+
+class SongBird(Bird):
+     def __init__(self):
+          # Bird.__init__(self)
+          super(SongBird,self).__init__()
+          self.sound = 'Squawk'
+     def sing(self):
+          print "gefkl;gjr;t"
+sb = SongBird()
+sb.sing()   #能正常输出
+sb.eat()     #报错，因为songgird中没有hungry特性
