02bd148c0cade848df7846bc81340b2863a3b21e
whyismefly <whyismeflying@hotmail.com>
2019-02-22 00:25:18 +0800

    Update 使用cookie获取网页.py

--- pachong/try/使用cookie获取网页.py
+++ pachong/try/使用cookie获取网页.py
@@ -9,6 +9,13 @@ cookie={"BDUSS":
 }
 req=requests.get(url,cookies=cookie)
 soup=BeautifulSoup(req.content,"lxml")
-with open("baidu.html","wb") as file:
-    file.write(soup.encode("utf-8"))
-    file.close()
+print soup
+#写法一
+# with open("baidu.html","wb") as file:
+#     file.write(soup.encode("utf-8"))
+#     file.close()
+
+#写法二
+# html1 = open( "baidu1.html", "wb")
+# html1.write(soup.encode("utf-8"))
+# html1.close()
