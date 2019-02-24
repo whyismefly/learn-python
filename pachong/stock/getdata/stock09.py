4b8a7513443256cccd58448c606d279e01066b2a
whyismefly <whyismeflying@hotmail.com>
2019-02-25 00:33:39 +0800

    20190225

--- pachong/stock/getdata/stock09.py
+++ pachong/stock/getdata/stock09.py
@@ -23,17 +23,17 @@ def getStackCode(html):
     return code
 
 Url = 'http://quote.eastmoney.com/stocklist.html'#东方财富网股票数据连接地址
-filepath = 'D:\\data\\'#定义数据文件保存路径
+filepath = 'E:\\learn\\GitHub\\learn-python\\pachong\\stock\\data\\'#定义数据文件保存路径
 #实施抓取
 code = getStackCode(getHtml(Url))
 #获取所有股票代码（以6开头的，应该是沪市数据）集合
 CodeList = []
 for item in code:
-    if item[0]=='6':
+    # if item[0]=='6':
         CodeList.append(item)
 #抓取数据并保存到本地csv文件
 for code in CodeList:
     print('正在获取股票%s数据'%code)
     url = 'http://quotes.money.163.com/service/chddata.html?code=0'+code+\
-        '&end=20161231&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;TURNOVER;VOTURNOVER;VATURNOVER;TCAP;MCAP'
-    urllib.request.urlretrieve(url, code+'.csv')
+        '&end=20190224&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;TURNOVER;VOTURNOVER;VATURNOVER;TCAP;MCAP'
+    urllib.request.urlretrieve(url, filepath + code + '.csv')
