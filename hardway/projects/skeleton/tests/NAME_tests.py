bd03ca6915c974bf601afc39141ea92ac72a2a6c
whyismefly <whyismeflying@hotmail.com>
2019-02-27 02:12:11 +0800

    20190227
    查缺补漏，加深目录和powershell操作印象

--- hardway/projects/skeleton/tests/NAME_tests.py
+++ hardway/projects/skeleton/tests/NAME_tests.py
@@ -1,11 +1,17 @@
+# encoding:utf-8
+
 from nose.tools import *
+import sys
+sys.path.append("E:\\learn\\GitHub\\learn-python\\hardway\\projects\\skeleton")
 import NAME
-sys.path.append("E:\learn\GitHub\learn-python\hardway\projects\skeleton")
-def setup() :
-    print "setup"
+#import NAME 不能放在import sys和sys.path.append("E:\\learn\\GitHub\\learn-python\\hardway\\projects\\skeleton")前面，否则在pycharm中执行失败，在powershell中则无需这两句话
+
+def setup():
+    print "SETUP!"
 
 def teardown():
-    print "tear down"
+    print "TEAR DOWN!"
 
 def test_basic():
-    print "i ran"
+    print "I RAN!"
+
