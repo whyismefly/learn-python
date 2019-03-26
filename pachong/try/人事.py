2b3226ec63702c792ae337ef12cf0ca483e1959d
whyismefly <whyismeflying@hotmail.com>
2019-03-26 17:45:14 +0800

    20190326

--- pachong/try/人事.py
+++ pachong/try/人事.py
@@ -5,12 +5,15 @@
 # import re
 # import urllib
 """
+
+#先安装html5lib
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 # import csv
 # import html5lib#使用read_html()时出现ImportError: html5lib not found, please install it
 from sqlalchemy import create_engine
+import datetime
 
 
 """
@@ -44,15 +47,32 @@ from sqlalchemy import create_engine
 # print h520
 """
 
+def getlist():
+    try:
+        url_first = "http://www.apta.gov.cn/Officer/Summary?examid=90&pi="
+        start_time = datetime.datetime.now()
+        print str(start_time)+" start"
+        for page in range(1, 56):  # 一共55页
+            url_all = url_first + str(page)
+            # req = requests.get(url_all)
+            # https: // blog.csdn.net / weixin_36646275 / article / details / 83965610
+            """HTTPConnectionPool（host:XX）Max retries exceeded with url 解决方法
+在做双十一压测时,高并发调用requests时报错.问题解决方法
+问题原因
+是因为在每次数据传输前客户端要和服务器建立TCP连接，为节省传输消耗，默认为keep-alive，即连接一次，传输多次，然而在多次访问后不能结束并回到连接池中，导致不能产生新的连接,headers中的Connection默认为keep-alive，将header中的Connection一项置为close.
+"""
+            req = requests.get(url_all,headers={"Connection": "close"})
+            soup = BeautifulSoup(req.text, "lxml")
+            tb = pd.read_html(soup.prettify(), header=0)[4]  # 经观察人事考试网中所需表格是网页中第5个表格，故为[4]
+            engine = create_engine('mysql://root:root@localhost:3306/stock_test?charset=utf8')
+            tb.to_sql('renshiwang2', engine, if_exists='append')
+            df1 = pd.read_sql('renshiwang2', engine)
+        end_time=datetime.datetime.now()
+        print str(end_time)+" LIST DONE"
+    except Exception,e:
+        end_time = datetime.datetime.now()
+        print str(end_time)+" Exception:"
+        print e
 
-url_first="http://www.apta.gov.cn/Officer/Summary?examid=90&pi="
-
-for page in range(1,56):#一共55页
-    url_all=url_first+str(page)
-    req = requests.get(url_all)
-    soup = BeautifulSoup(req.text, "lxml")
-    tb = pd.read_html(soup.prettify(),header = 0)[4]  # 经观察人事考试网中所需表格是网页中第5个表格，故为[4]
-    engine = create_engine('mysql://root:root@localhost:3306/stock_test?charset=utf8')
-    tb.to_sql('renshiwang2', engine, if_exists='append')
-    df1 = pd.read_sql('renshiwang2', engine)
-
+if __name__ == '__main__':
+    getlist()
