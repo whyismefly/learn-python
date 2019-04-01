ba04be7e2fb34979e7d014ed4bcf04a3bafa6379
whyismefly <whyismeflying@hotmail.com>
2019-04-02 01:19:46 +0800

    Update stock10.py
    看仔细点，两分钟解决的问题拖几天

--- pachong/stock/getdata/stock10.py
+++ pachong/stock/getdata/stock10.py
@@ -33,7 +33,7 @@ engine = create_engine('mysql://root:root@localhost:3306/stock_test?charset=utf8
 https://blog.csdn.net/helloxiaozhe/article/details/83018347
 error1170 也会出现，这个问题出现在当你设置一个VARCHAR字段为主键，但是却错误的设置了长度或者字符数，事实上，VARCHAR只能接受最大为256个字符串，但是你错误的设置成VARCHAR(512)等一些错误的设置，这些错误的设置会强制MySQL自动将VARCHAR(512)等转换成SMALLINT类型，同时这个字段被设置成primary key ，unique限制或者index索引等，然后执行操作就出现error 1170错误，解决问题的方法，为VARCHAR域指定小于256的长度。
 """
-# stock_hist_data.to_sql(stockid+"_hist_data"+str(today),engine,if_exists='append',dtype={'code':sqlalchemy.types.VARCHAR(length=6)})
+# stock_hist_data.to_sql(stockid+"_hist_data"+str(today),engine,if_exists='append',dtype={'date':sqlalchemy.types.VARCHAR(length=20)})
 # #自己设定的VARCHAR长度，可修改，建议使用下行这种形式
-stock_hist_data.to_sql(stockid+"_hist_data"+str(today),engine,if_exists='append',dtype={'code':sqlalchemy.types.VARCHAR(stock_hist_data.index.get_level_values("date").str.len().max())})
+stock_hist_data.to_sql(stockid+"_hist_data",engine,if_exists='append',dtype={'date':sqlalchemy.types.VARCHAR(stock_hist_data.index.get_level_values("date").str.len().max())})
 
