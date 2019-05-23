7a5a24ad4a905fa2ed02f31b63098653835058f3
whyismefly <whyismeflying@hotmail.com>
2019-05-23 18:26:08 +0800

    20190523

--- pachong/work/data/test07.py
+++ pachong/work/data/test07.py
@@ -36,6 +36,8 @@ Upgrade-Insecure-Requests: 1
 User-Agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36
 """
 
+
+"""
 LOGIN_URL = 'http://yingxiao.chewumi.com/login.php'  #请求的URL地址
 DATA = {"username":'accountID',"passwd":'passwd'}   #登录系统的账号密码,也是我们请求数据
 
@@ -44,3 +46,32 @@ HEADERS = {
            }
 RES = requests.post(LOGIN_URL,data=DATA,headers=HEADERS)  #模拟登陆操作
 print (RES.text) #打印返回的文本信息
+"""
+
+
+LOGIN_URL="http://134.64.116.90:8030/sdp/p1521813509851_AutoCreateZul_207.zul;Managed_Server_Name=134.64.70.56:8020"
+Cookies="""JSESSIONID=F3A178815DBA239BA7C2397D55E238D3; Managed_Server_Name="134.64.70.56:8020"; PHPSESSID=7d52ff757b92; BIGipServerpool_nbtyrz1=544489606.28451.0000; jsessionid_zHSp=Orjekdq5gUoGYlUPh4mE8TPZRUYWz59Uq193nhvS53n25Zpk1Yts!-791586314; PORTAL_SID=rvBXcldDp41hFjMcmjdfkMQFvJkk5J3YjrhdDNgTGWJFhfmsh6Nb!-723442693; IDEALSEARCH_AH=0xylcmPNZyt3pllnWQ94Rs7NvWQGGw62L8qv81cvy2HSyY2XvZf6!961753984; JSESSIONID-LDC=n22bcmVW5JLGxCyVl1Q7S1MvN9lSJsLlGk4fpTSTN5z7CJHB1GpM!560399883; JSESSIONID=8zDZcmFdCZ7hkHDhzxvhr1mhMRqGQlwR23QhRhfmRH2vKxpcWQyQ!120989381"""
+Cookie={'JSESSIONID':'F3A178815DBA239BA7C2397D55E238D3',
+        ' Managed_Server_Name':"134.64.70.56:8020",
+        'PHPSESSID':'7d52ff757b92',
+        'BIGipServerpool_nbtyrz1':'544489606.28451.0000',
+        'jsessionid_zHSp':'Orjekdq5gUoGYlUPh4mE8TPZRUYWz59Uq193nhvS53n25Zpk1Yts!-791586314',
+        'PORTAL_SID':'rvBXcldDp41hFjMcmjdfkMQFvJkk5J3YjrhdDNgTGWJFhfmsh6Nb!-723442693',
+        'IDEALSEARCH_AH':'0xylcmPNZyt3pllnWQ94Rs7NvWQGGw62L8qv81cvy2HSyY2XvZf6!961753984',
+        'JSESSIONID-LDC':'n22bcmVW5JLGxCyVl1Q7S1MvN9lSJsLlGk4fpTSTN5z7CJHB1GpM!560399883',
+        'JSESSIONID':'8zDZcmFdCZ7hkHDhzxvhr1mhMRqGQlwR23QhRhfmRH2vKxpcWQyQ!120989381"'}
+# RequestHeaders={Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
+# Accept-Encoding:gzip, deflate
+# Accept-Language:zh-CN,zh;q=0.9
+# Cache-Control:max-age=0
+# Connection:keep-alive
+# Cookie:JSESSIONID=F3A178815DBA239BA7C2397D55E238D3; Managed_Server_Name="134.64.70.56:8020"; PHPSESSID=7d52ff757b92; BIGipServerpool_nbtyrz1=544489606.28451.0000; jsessionid_zHSp=Orjekdq5gUoGYlUPh4mE8TPZRUYWz59Uq193nhvS53n25Zpk1Yts!-791586314; PORTAL_SID=rvBXcldDp41hFjMcmjdfkMQFvJkk5J3YjrhdDNgTGWJFhfmsh6Nb!-723442693; IDEALSEARCH_AH=0xylcmPNZyt3pllnWQ94Rs7NvWQGGw62L8qv81cvy2HSyY2XvZf6!961753984; JSESSIONID-LDC=n22bcmVW5JLGxCyVl1Q7S1MvN9lSJsLlGk4fpTSTN5z7CJHB1GpM!560399883; JSESSIONID=8zDZcmFdCZ7hkHDhzxvhr1mhMRqGQlwR23QhRhfmRH2vKxpcWQyQ!120989381
+# Host:134.64.116.90:8030
+# Referer:http://134.64.116.90:9185/ldc/home.jsp
+# Upgrade-Insecure-Requests:1
+# User-Agent:Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36}
+req=requests.get(LOGIN_URL,cookies=Cookie)
+# print req.cookies
+# print type(req.cookies)
+print req.text
+# print Cookies
