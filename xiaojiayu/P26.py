9b5408e7bb87a75d971c509110ff1d9c85695d31
whyismefly <whyismeflying@hotmail.com>
2019-06-07 00:47:38 +0800

    Create P26.py

---
+++ xiaojiayu/P26.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# encoding:utf-8
+
+brand=['bfuigfqw','fweofjpoef','fjoerwjf','fjowejfe',]
+slogan=['fhwejf','jofjpokbpd','ifepofifejg','km[pvp[f[]']
+print ('abd:',slogan[brand.index('fjowejfe')])
+
+dict1={'bfuigfqw':'fhwejf','fweofjpoef':'jofjpokbpd','fjoerwjf':'ifepofifejg','fjowejfe':'km[pvp[f[]'}
+print (dict1)
+print (dict1['fjoerwjf'])
+#只要建立了映射关系即可
+dict2=dict((('f',70),('i',105)))
+print dict2
+dict4=dict(gjoefj='fjowengre',frepkgr='goejge')
+print dict4
