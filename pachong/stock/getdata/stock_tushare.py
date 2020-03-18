d7cf90d83d7d400c0bbcc051da73552363cfb4cc
whyismefly <whyismeflying@hotmail.com>
2020-03-18 10:07:25 +0800

    Update stock_tushare.py

--- pachong/stock/getdata/stock_tushare.py
+++ pachong/stock/getdata/stock_tushare.py
@@ -1,22 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # encoding:utf-8
 
 import tushare as ts
 import pandas as pd
 from pandas import Series,DataFrame
 import lxml
-from sqlalchemy import create_engine#这个不好用，一堆报错，还是用下面的官方工具吧
-import mysql.connector
+# from sqlalchemy import create_engine#这个不好用，一堆报错，还是用下面的官方工具吧
+# import mysql.connector
 #matplotlib和seaborn都是比较常用的绘图工具，前者更加官方，后者好看更适合新手
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib
+# import matplotlib.pyplot as plt
+# import numpy as np
 
 #参考https://www.cnblogs.com/onemorepoint/p/7210789.html
 
-# print ts.get_hist_data('600848') #一次性获取全部日k线数据
-# df = ts.get_hist_data('000001',start='2017-01-01',end='2019-02-28')
-# print df.head(10)
+print (ts.get_hist_data('600848')) #一次性获取全部日k线数据
+df = ts.get_hist_data('000001',start='2017-01-01',end='2019-02-28')
+print (df.head(10))
 # print type(df)
 
 # stock_info=ts.get_stock_basics()#大盘当日所有股票数据
