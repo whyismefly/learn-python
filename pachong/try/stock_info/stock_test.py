eaae17a1c44c1b4e2a8f3c52fbea2c3d57122fdd
whyismefly <whyismeflying@hotmail.com>
2020-03-20 16:24:35 +0800

    Update stock_test.py

--- pachong/try/stock_info/stock_test.py
+++ pachong/try/stock_info/stock_test.py
@@ -4,29 +4,43 @@
 import tushare as ts
 import pandas as pd
 from pandas import Series,DataFrame
-# from sqlalchemy import create_engine
-# from sqlalchemy import VARCHAR
 from sqlalchemy import *
-
-
+import time
 
 def get_stocks_hist(stock_list):
-    stock_hist = ts.get_hists(stock_list,start="2019-01-01",end="2020-03-17")
-    print(stock_hist)
-    engine = create_engine('mysql://root:root@localhost:3306/stock_info?charset=utf8')
-    stock_hist.to_sql('stockhistinfo', engine, if_exists='append')
-    # stock_hist.to_sql('stockhistinfo', engine, if_exists='append',dtype={"date":Date()})
-    # stock_hist.to_sql('stockhistinfo', engine, if_exists='append', dtype={'date': VARCHAR(stock_hist.index.get_level_values('date').str.len().max())})
-    df1 = pd.read_sql('stockhistinfo', engine)
-    print(df1)
+    for stock_code in stock_list:
+        stock_hist = ts.get_hist_data(stock_code)
+        stock_hist["code"] = stock_code
+        # print(stock_hist)
+        engine = create_engine('mysql://root:root@localhost:3306/stock_info?charset=utf8')
+        stock_hist.to_sql('stockhistinfo', engine, if_exists='append',
+                          dtype={'date': VARCHAR(stock_hist.index.get_level_values('date').str.len().max()),'code':VARCHAR(length=6)})
+        # df1 = pd.read_sql('stockhistinfo', engine)
+        # print(df1)
+
 
 def get_basic_stockinfo_to_mysql():
     stock_info = ts.get_stock_basics()  # 大盘当日所有股票数据
     print(stock_info)
     engine = create_engine('mysql://root:root@localhost:3306/stock_info?charset=utf8')
-    stock_info.to_sql('stockinfo',engine,if_exists='append',dtype={'code':VARCHAR(length=6)})
+    stock_info.to_sql('stockinfo',engine,if_exists='append',dtype={'name':NVARCHAR(length=16),'code':VARCHAR(length=6)})
     df1 = pd.read_sql('stockinfo',engine)
     print (df1)
 
-stock_list=["603019","002456","300232","600216","300296","002252"]
-get_stocks_hist(stock_list)
+def open_list(doc_path_name):
+    with open(doc_path_name,mode='r',encoding="utf-8") as f:
+        info = f.read()
+        print(info)
+        return info
+
+
+# stock_list=["603019","002456","300232","600216","300296","002252"]
+# stock_list="603019"
+# get_stocks_hist(stock_list)
+doc_path_name="G:/Desktop/code.txt"
+open_list(doc_path_name)
+# get_basic_stockinfo_to_mysql()
+
+# now=time.strftime('%Y-%m-%d',time.localtime(time.time()))
+# print(now)
+# get_basic_stockinfo_to_mysql
