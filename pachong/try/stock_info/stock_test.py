f444d306a8841b7ef88efbb3e8761feb78de1659
whyismefly <whyismeflying@hotmail.com>
2020-03-26 16:21:56 +0800

    Update stock_test.py

--- pachong/try/stock_info/stock_test.py
+++ pachong/try/stock_info/stock_test.py
@@ -18,20 +18,15 @@ def get_stocks_hist(stock_list):
         # df1 = pd.read_sql('stockhistinfo', engine)
         # print(df1)
 
-
 def get_basic_stockinfo_to_mysql():
     stock_info = ts.get_stock_basics()  # 大盘当日所有股票数据
     print(stock_info)
     engine = create_engine('mysql://root:root@localhost:3306/stock_info?charset=utf8')
-    stock_info.to_sql('stockinfo',engine,if_exists='append',dtype={'name':NVARCHAR(length=16),'code':VARCHAR(length=6)})
+    stock_info.to_sql('stockinfo',engine,if_exists='append',dtype={'name':NVARCHAR(length=32),'code':VARCHAR(length=6)})
     df1 = pd.read_sql('stockinfo',engine)
     print (df1)
 
 def open_list(doc_path_name):
-    # with open(doc_path_name,mode='r',encoding="utf-8") as f:
-    #     info = f.read()
-    #     print(info)
-    #     return info
     with open(doc_path_name,mode='r',encoding="utf-8") as f:
         info = f.read()
         code_list=[]
@@ -41,14 +36,13 @@ def open_list(doc_path_name):
         print(code_list)
         return code_list
 
-
 # stock_list=["603019","002456","300232","600216","300296","002252"]
 # stock_list="603019"
-doc_path_name="G:/Desktop/code.txt"
-stock_list=open_list(doc_path_name)
-get_stocks_hist(stock_list)
+# doc_path_name="G:/Desktop/code.txt"
+# stock_list=open_list(doc_path_name)
+# get_stocks_hist(stock_list)
 
-# get_basic_stockinfo_to_mysql()
+get_basic_stockinfo_to_mysql()
 
 # now=time.strftime('%Y-%m-%d',time.localtime(time.time()))
 # print(now)
